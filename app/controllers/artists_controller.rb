6e47f142edb664bf1d2f1b43200cb9ce096d9b12
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-15 20:17:24 -0800

    tests pass

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -41,10 +43,17 @@ class ArtistsController < ApplicationController
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
   end
+  
+  def song_count
+    @artist.songs.count
+  end
 
   private
 
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  
+
 end
